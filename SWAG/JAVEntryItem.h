7cce9d0512f3ff8080d53b53f88e9cbfec41b2d7
Javier Osorio <josorio@hsds5553.nyc16w22.hsbgroup.com>
2014-08-25 17:48:43 -0400

    Entry item class.

---
+++ SWAG/JAVEntryItem.h
@@ -0,0 +1,13 @@
+//
+//  JAVEntryItem.h
+//  SWAG
+//
+//  Created by Javier Osorio on 8/25/14.
+//  Copyright (c) 2014 JavHimself. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JAVEntryItem : NSObject
+
+@end
---
+++ SWAG/JAVEntryItem.m
@@ -0,0 +1,13 @@
+//
+//  JAVEntryItem.m
+//  SWAG
+//
+//  Created by Javier Osorio on 8/25/14.
+//  Copyright (c) 2014 JavHimself. All rights reserved.
+//
+
+#import "JAVEntryItem.h"
+
+@implementation JAVEntryItem
+
+@end
