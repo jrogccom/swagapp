cb67713389dc13d1297e3428bd45052f3eca9f07
Javier 2 <Admin2@Javiers-MacBook-Pro.local>
2014-08-25 23:48:27 -0400

    NSCoding protocol methods. 	modified:   SWAG/JAVEntryItem.m

--- SWAG/JAVEntryItem.m
+++ SWAG/JAVEntryItem.m
@@ -14,4 +14,27 @@
 {
     return (NSDate *)_checkoutHistory.lastObject;
 }
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super init];
+    if (self) {
+        self.title = [aDecoder decodeObjectForKey:@"title"];
+        self.publisher = [aDecoder decodeObjectForKey:@"publisher"];
+        self.authors = [aDecoder decodeObjectForKey:@"authors"];
+        self.categories = [aDecoder decodeObjectForKey:@"categories"];
+        self.checkoutHistory = [aDecoder decodeObjectForKey:@"checkoutHistory"];
+        self.entryNumber = [aDecoder decodeObjectForKey:@"entryNumber"];
+    }
+    return self;
+}
+- (void)encodeWithCoder:(NSCoder *)coder
+{
+    [coder encodeObject:self.title forKey:@"title"];
+    [coder encodeObject:self.publisher forKey:@"publisher"];
+    [coder encodeObject:self.categories forKey:@"categories"];
+    [coder encodeObject:self.entryNumber forKey:@"entryNumber"];
+    [coder encodeObject:self.checkoutHistory forKey:@"checkoutHistory"];
+    [coder encodeObject:self.authors forKey:@"authors"];
+}
 @end
