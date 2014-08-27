bbc84ee48961e24a6c71976f834f256531f47315
Javier Osorio <josorio@hsds5553.nyc16w22.hsbgroup.com>
2014-08-27 14:56:23 -0400

    AFRESTClient subclass with AFIncrementalHTTLClient protocol implementations/overrides. 	new file:   SWAG/SWAGRESTClient.h 	new file:   SWAG/SWAGRESTClient.m

---
+++ SWAG/SWAGRESTClient.h
@@ -0,0 +1,13 @@
+//
+//  SWAGRESTClient.h
+//  SWAG
+//
+//  Created by Javier Osorio on 8/27/14.
+//  Copyright (c) 2014 JavHimself. All rights reserved.
+//
+
+#import "AFRESTClient.h"
+
+@interface SWAGRESTClient : AFRESTClient <AFIncrementalStoreHTTPClient>
+
+@end
